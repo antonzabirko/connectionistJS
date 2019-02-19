cec61feaf0925c4707eadd489be969f212e0de7e
Anton Zabirko <antonzabirko@outlook.com>
2019-02-19 09:58:04 -0500

    sigmoid finished

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import http from 'http';
 import math from 'mathjs';
 import _ from 'lodash';
+import nj from 'numjs';
 
 const app = express(),
 			server = http.createServer(app),
@@ -18,86 +19,90 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.get('/', (req, res) => res.send('Hello World!'))
 
 
-console.log(math.sqrt(9));
+console.log(nj.array([2, 3, 4]));
 
 /**
  *  numjs - array
  */
-
-class nj {
-		/**
-		 *  Creates an array
-		 *
-		 */
-		array(arr) {
-				this.values = arr;
-				this.dimensions = dim(arr);
-		}
-
-		/**
-		 *  Counts dimensions in an array
-		 *  @params Array
-		 *  @return int
-		 */
-		countDim(arr) {
-				if (!arr.length) {
-						return 0; // current array lacks dimensions
-				}
-				return 1 + this.dim(arr[0]);
-		}
-
-		/**
-		 *  Evaluates to boolean the equality of an array
-		 *  @params Array
-		 *
-		 *  [
-		 *      [
-		 *          [1, 2],
-		 *          [2, 3]
-		 *      ],
-		 *      [
-		 *          [1, 4],
-		 *          [6, 4]
-		 *      ]
-		 *  ]
-		 */
-		isDimensionBalanced(arr) {
-				let arrFlat = _.flattenDeep(arr);
-
-				// Test that all types are identical
-				arrFlat.every((v, i, a) => {
-						return i === 0 || typeof v === typeof a[i - 1];
-				});
-
-				if (!arr[0]) {
-						for (let i = 0; i <= arr.length; i++) {
-								if (arr[0].length !== arr[1].length) {
-										return false; // current dimension is unbalanced
-								}
-						}
-				}
-				return this.areBalanced(arr[0]);
-		}
-
-		/**
-		 *  Exponentiates an array or matrix
-		 *
-		 */
-		exp() {
-
-				//return math.divide(1, math.add(1, math.expm(z)));
-		}
-}
-let arr = 5;
-
-console.log(nj.prototype.dim(arr));
+//
+// class nj_local {
+// 		/**
+// 		 *  Creates an array
+// 		 *
+// 		 */
+// 		array(arr) {
+// 				this.values = arr;
+// 				this.dimensions = dim(arr);
+// 		}
+//
+// 		/**
+// 		 *  Counts dimensions in an array
+// 		 *  @params Array
+// 		 *  @return int
+// 		 */
+// 		countDim(arr) {
+// 				if (!arr.length) {
+// 						return 0; // current array lacks dimensions
+// 				}
+// 				return 1 + this.dim(arr[0]);
+// 		}
+//
+// 		/**
+// 		 *  Evaluates to boolean the equality of an array
+// 		 *  @params Array
+// 		 *
+// 		 *  [
+// 		 *      [
+// 		 *          [1, 2],
+// 		 *          [2, 3]
+// 		 *      ],
+// 		 *      [
+// 		 *          [1, 4],
+// 		 *          [6, 4]
+// 		 *      ]
+// 		 *  ]
+// 		 */
+// 		isDimensionBalanced(arr) {
+// 				let arrFlat = _.flattenDeep(arr);
+//
+// 				// Test that all types are identical
+// 				arrFlat.every((v, i, a) => {
+// 						return i === 0 || typeof v === typeof a[i - 1];
+// 				});
+//
+// 				if (!arr[0]) {
+// 						for (let i = 0; i <= arr.length; i++) {
+// 								if (arr[0].length !== arr[1].length) {
+// 										return false; // current dimension is unbalanced
+// 								}
+// 						}
+// 				}
+// 				return this.areBalanced(arr[0]);
+// 		}
+//
+// 		/**
+// 		 *  Exponentiates an array or matrix
+// 		 *
+// 		 */
+// 		exp() {
+//
+// 				//return math.divide(1, math.add(1, math.expm(z)));
+// 		}
+// }
+// let arr = 5;
+//
+// console.log(nj.prototype.dim(arr));
 
 /**
  *  Sigmoid
+ *
+ *  @params Array z
  */
 
 function sigmoid(z) {
-		return math.divide(1, math.add(1, math.expm(z)));
+	let ones = nj.ones(z.shape);
+
+	return ones.divide(ones.add(nj.exp(z)));
 }
 
 /**
@@ -120,5 +125,5 @@ class recurrentNeuralNetwork {
 		}
 }
 
-let rnn2 = new recurrentNeuralNetwork(5);
+let rnn2 = new recurrentNeuralNetwork(2, 5, 1);
 console.log(rnn2);
