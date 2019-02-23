a285c3ee7fe78b9a3f8ae2b10ef92185c4e26b08
Anton <antonzabirko@outlook.com>
2019-02-22 19:06:09 -0500

    RNN -> ANN development

--- server.js
+++ server.js
@@ -23,10 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
  *  DEBUGS
  */
 
-let x = nj.array([[9, 0], [-3, 8]]);
+let x = nj.array([[9, 0], [-3, 8]]).divide(3);
 
-console.log(sigmoidPrime(x));
-//console.log(sigmoid(x));
 //console.log(x.pow(2));
 
 
@@ -43,19 +41,20 @@ function neg(z) {
 }
 
 /**
- *  RNN
+ *  ANN
  */
 
-class RNN {
-    constructor(inputLayerSize, outputLayerSize, hiddenLayerSize) {
+class ANN {
+    constructor(inputLayerSize, hiddenLayerSize, outputLayerSize, Lambda) {
         // Define Hyperparameters
         this.inputLayerSize = inputLayerSize;
         this.outputLayerSize = outputLayerSize;
         this.hiddenLayerSize = hiddenLayerSize;
+        this.Lambda = Lambda;
 
         // Define Weights (parameters)
-        this.weightA = nj.random(this.inputLayerSize, this.hiddenLayerSize);
-        this.weightB = nj.random(this.hiddenLayerSize, this.outputLayerSize);
+        this.wA = nj.random(this.inputLayerSize, this.hiddenLayerSize);
+        this.wB = nj.random(this.hiddenLayerSize, this.outputLayerSize);
     }
 
     /**
@@ -64,7 +63,6 @@ class RNN {
      *  @params NdArray z
      *  @return NdArray
      */
-
     sigmoid(z) {
         let ones = nj.ones(z.shape);
 
@@ -72,14 +70,31 @@ class RNN {
     }
 
     /**
-     *  Move forward in the RNN
+     *  Move the inputs forward in the network
      */
     forward(X) {
-        this.z2 = nj.dot(X, this.weightA);
-        this.a2 = sigmoid(this.z2);
-        this.z3 = nj.dot(this.a2, this.weightB);
+        this.z2 = nj.dot(X, this.wA);
+        this.a2 = this.sigmoid(this.z2);
+        this.z3 = nj.dot(this.a2, this.wB);
+        this.yHat = this.sigmoid(this.z3);
 
-        return sigmoid(this.z3);
+        return this.yHat;
+    }
+
+    /**
+     * Cost Function
+     *
+     * @param X
+     * @param Y
+     * @returns {Unit|*}
+     */
+    costFunction(X, Y) {
+        // Compute cost for X and Y using the weights already stored
+        this.yHat = this.forward(X);
+        let J = (0.5 * (nj.sum(Y.subtract(this.yHat).pow(2)))).divide(X.shape[0].add(this.Lambda.divide(2)
+            .multiply(this.wA.pow(2).sum().add(this.wB.pow(2).sum()))));
+
+        return J;
     }
 
     /**
@@ -88,13 +103,12 @@ class RNN {
      *  @params Array input
      *  @return Array
      */
-
-    costFunctionPrime(X, y) {
+    costFunctionPrime(X, Y) {
         this.yHat = this.forward(X);
-        this.delta3 = (neg(y.subtract(this.yHat))).multiply(sigmoidPrime(this.z3));
-        this.dJdW2 = (this.a2.T).dot(this.delta3);
-        this.delta2 = (this.delta3.dot(this.weightB.T)) * (sigmoidPrime(this.z2));
-        this.dJdW1 = (this.delta2).dot(X.T);
+        this.delta3 = (neg(Y.subtract(this.yHat))).multiply(this.sigmoidPrime(this.z3));
+        this.dJdW2 = (this.a2.T).dot(this.delta3).divide(X.shape[0].add(this.Lambda.multiply(this.wB)));
+        this.delta2 = (this.delta3.dot(this.wB.T)).multiply((this.sigmoidPrime(this.z2)));
+        this.dJdW1 = (this.delta2).dot(X.T).divide(X.shape[0].add(this.Lambda.multiply(this.wA)));
 
         return [this.dJdW1, this.dJdW2];
     }
@@ -105,25 +119,42 @@ class RNN {
      *  @params NdArray z
      *  @return NdArray
      */
-
     sigmoidPrime(z) {
         return nj.exp(neg(z)).divide(nj.exp(neg(z)).add(1).pow(2));
     }
+
+    computeGradients(X, Y) {
+        let costResults = this.costFunctionPrime(X, Y);
+        let dJdW1 = costResults[0];
+        let dJdW2 = costResults[1];
+
+        return nj.concat(dJdW1.flatten(), dJdW2.flatten());
+    }
+
+    /**
+     *  Helper Functions for
+     */
 }
 
-class trainer {
+class Trainer {
     constructor(N) {
+        // Link to the ANN
         this.N = N;
     }
 
-    costFunctionWrapper(params, X, y) {
+    costFunctionWrapper(params, X, Y) {
         this.N.setParams(params);
     }
 
-    train(fnc, grd, x0) {
-        this._res = optimjs.minimize_L_BFGS(fnc, grd, x0);
+    train(X, Y) {
+        this._res = optimjs.minimize_L_BFGS(N.costFunction(X, Y), N.computeGradients(X, Y), X);
     }
 }
 
-let model = new RNN(2, 3, 1);
+let NN = new ANN(2, 3, 1, 0.0001),
+    T = new Trainer(NN),
+    X = nj.array([[.3, 1], [.5, .2], [1, .4]]),
+    Y = nj.array([75, 82, 93]).divide(100);
+    //X = X.divide(nj.amax(X));
+console.log(Y);
 //console.log(model.forward(x));
